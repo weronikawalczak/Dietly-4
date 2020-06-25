ffcacb30b78f93eed820500dc853b0197c3dd23a
weronikawalczak989 <weronikawalczak989@gmail.com>
2020-06-25 15:57:23 +0200

    Initial app with dependencies

---
+++ src/test/java/com/weronika/dietly/DietlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weronika.dietly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
