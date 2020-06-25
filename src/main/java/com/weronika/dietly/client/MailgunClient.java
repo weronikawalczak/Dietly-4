1a61accb3d265912a700239e83bdc1399969133e
weronikawalczak989 <weronikawalczak989@gmail.com>
2020-06-25 18:39:33 +0200

    More than one client bean

--- src/main/java/com/weronika/dietly/client/MailgunClient.java
+++ src/main/java/com/weronika/dietly/client/MailgunClient.java
@@ -5,20 +5,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Component("MailChimpClient")
-public class MailChimpClient implements ExternalClient{
+@Component("MailgunClient")
+public class MailgunClient implements ExternalClient{
 
-    @Value("${dietly.mailchimp.url:http://mailchimp/api}")
-    private String mailChimpApiUrl;
+    @Value("${dietly.mailgun.url:http://mailgun/api}")
+    private String mailGunApiUrl;
 
     private final RestTemplate restTemplate;
 
-    public MailChimpClient(RestTemplate restTemplate) {
+    public MailgunClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
     public void sendMessage(Message message) {
-        restTemplate.postForObject(mailChimpApiUrl, message, Message.class);
+        restTemplate.postForObject(mailGunApiUrl, message, Message.class);
     }
 }
--- src/main/java/com/weronika/dietly/service/MessageService.java
+++ src/main/java/com/weronika/dietly/service/MessageService.java
@@ -3,6 +3,7 @@ package com.weronika.dietly.service;
 import com.weronika.dietly.client.ExternalClient;
 import com.weronika.dietly.entity.Message;
 import com.weronika.dietly.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,7 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final ExternalClient externalClient;
 
-    public MessageService(MessageRepository messageRepository, ExternalClient externalClient) {
+    public MessageService(MessageRepository messageRepository, @Qualifier("MailChimpClient") ExternalClient externalClient) {
         this.messageRepository = messageRepository;
         this.externalClient = externalClient;
     }
